55ac9d0b50af98bb5c03115e2ee6ace5eaa3ddc5
hyouuu <hyouuu@gmail.com>
2021-04-04 03:25:57 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,19 @@
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
     name: "DiffMatchPatch",
-    targets: [Target(name: "DiffMatchPatch", dependencies:["diff_match_patch"])]
+    products: [
+        // The external product of our package is an importable
+        // library that has the same name as the package itself:
+        .library(
+            name: "DiffMatchPatch",
+            targets: ["DiffMatchPatch"]
+        )
+    ],
+    targets: [
+        // Our package contains two targets, one for our library
+        // code, and one for our tests:
+        .target(name: "DiffMatchPatch", dependencies:["diff_match_patch"])
+    ]
 )
